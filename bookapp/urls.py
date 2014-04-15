970ed9712f83f05681894c770170a7448194dacb
zhuwowuyuing <zhuwowuyuing@gmail.com>
2014-04-15 18:23:22 +0800

    del django-groupwork files

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -1,12 +1,9 @@
 # from django.conf.urls.defaults import *
-from models import *
 from django.conf.urls import patterns, include, url
 
-from views import *
-
 urlpatterns = patterns('',
-    (r'book/create/$', create_book),
-    (r'book/list/$', list_book ),
-    (r'book/edit/(?P<id>[^/]+)/$', edit_book),
-    (r'book/view/(?P<id>[^/]+)/$', view_book),
+    url(r'book/create/$', 'bookapp.views.create_book'),
+    url(r'book/list/$', 'bookapp.views.list_book'),
+    url(r'book/edit/(?P<id>[^/]+)/$', 'bookapp.views.edit_book'),
+    url(r'book/view/(?P<id>[^/]+)/$', 'bookapp.views.view_book'),
 )
